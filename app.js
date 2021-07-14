c82f595e0381bf4ef67ef37310295ed061ff99f2
LeenKaraja <>
2021-07-14 13:32:07 +0000

    edit

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ var name = "hala"
 
 
 
-alert("hi, welcome hala" + name)
+alert("hi, welcome " + name)
 
 
 var name = prompt("what is your name")
@@ -50,16 +50,18 @@ console.log(1 == 1 && 1>0)
 console.log(1 == 9 && 1>0)
 console.log(1 == 9 || 1>0)
 
-if(1<0){
-  console.log('yes it is')
+if(favCar == "tesla"){
+ document.write("<div>" + "<h4>" + favCar + "</h4>" + "<img src='https://www.teslarati.com/wp-content/uploads/2020/06/tesla-model-s-plaid-3-1024x670.jpg' 'alt=tesla'
+  'width=300' 'height=200' 'title=tesla'>" + "</div")
+}
+if else(favCar == "mazda"){
+  document.write("<div>" + "<h4>" + favCar + "</h4>" + "<img src='https://images.hgmsites.net/lrg/2021-mazda-mazda6_100770171_l.jpg' 'alt=Mazda' 'width=300' 'height=200' 'title=mazda'>" + "</div>")
+
+}
+if else(favCar == "mercedes"){
+ document.write("<div>" + "<h4>" + favCar + "</h4>" + "<img src='https://www.mercedes-benz.com/en/company/_jcr_content/root/slider/sliderchilditems/slideritem/image/MQ3-0-tabletImage-20191025121730/01-mercedes-benz-ag-company-3400x1440.jpeg' 'alt=mercedes' 'width=300' 'height=200' 'title=mercedes'>" + "</div>")
 }
 else{
-  console.log("no it is")
+  console.log("Your fav car is not here")
 }
 
-if(1<0 || 1==1){
-  console.log('yes it is')
-}
-else{
-  console.log("no it is")
-}
