b385cf2d2860037c8c4d8dd2dec8de3910c312fc
LeenKaraja <>
2021-07-14 10:23:40 +0000

    edit4

---
+++ app.js
@@ -0,0 +1,65 @@
+
+var firstName = "Leen" 
+
+console.log(firstName)
+
+console.log("hello")
+
+var x = '123 ! @ leemjtbd'
+
+console.log(x)
+console.log(1+1 , 2>5 , 7<10)
+
+var num = 10
+var num2 = 20
+var concat = firstName + " " + num
+var sum = num + num2 
+ console.log(concat , "sum result" , sum)
+
+var isCreater = true 
+
+
+var z =5
+
+var y = z + 10 
+
+console.log(z , "y=" , y) 
+
+console.log(1==1 , 1=='1', 1=='12')
+
+console.log(1===1, 1==='1')
+
+alert("hi, welcome")
+
+var name = "hala"
+
+
+
+alert("hi, welcome hala" + name)
+
+
+var name = prompt("what is your name")
+
+var favCar = prompt("what is your fav car")
+
+document.write('<p>' + favCar + '</p>')
+
+document.write("welcome to javascript")
+
+console.log(1 == 1 && 1>0)
+console.log(1 == 9 && 1>0)
+console.log(1 == 9 || 1>0)
+
+if(1<0){
+  console.log('yes it is')
+}
+else{
+  console.log("no it is")
+}
+
+if(1<0 || 1==1){
+  console.log('yes it is')
+}
+else{
+  console.log("no it is")
+}
